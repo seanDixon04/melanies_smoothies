51bd1a66a6e3a532f273f3f1a0308680cb645859
seanDixon04 <163200095+seanDixon04@users.noreply.github.com>
2025-04-02 22:57:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,8 +48,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-if smoothiefroot_response.status_code == 200:
-    st.text(smoothiefroot_response.json())
-else:
-    st.error("Failed to retrieve data from API.")
